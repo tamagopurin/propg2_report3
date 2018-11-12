f867dde93926cda5018227bf26618e64cae35b6b
K Yamashiro <e185728@ie.u-ryukyu.ac.jp>
2018-11-13 03:30:21 +0900

    report3 修正前

--- src/test/jp/ac/uryukyu/ie/e185728/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e185728/EnemyTest.java
@@ -8,5 +8,13 @@ class EnemyTest {
 
     @Test
     void attack() {
+        int heroHP = 10;
+        Hero hero = new Hero("testHero", 8, 4);
+        Enemy enemy = new Enemy("testEnemy", 6, 2);
+        enemy.dead = true;
+        for (int i = 0; i < 10; i++) {
+            enemy.attack(hero);
+        }
+        assertEquals(heroHP, hero.hitPoint);
     }
 }
