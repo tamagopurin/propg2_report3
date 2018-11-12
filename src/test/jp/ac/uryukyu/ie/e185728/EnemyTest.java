6429716529d9245b0ef77819d8cb4dffb2531feb
K Yamashiro <e185728@ie.u-ryukyu.ac.jp>
2018-11-13 02:42:13 +0900

    report3 修正前

---
+++ src/test/jp/ac/uryukyu/ie/e185728/EnemyTest.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e185728;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    void attack() {
+    }
+}
