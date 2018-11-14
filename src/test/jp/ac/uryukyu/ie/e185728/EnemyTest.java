cf38f76ebe6c7b10aa1b88cf1c27ea06fbf7fa56
K Yamashiro <e185728@ie.u-ryukyu.ac.jp>
2018-11-14 11:59:13 +0900

    report3 修正前

--- src/test/jp/ac/uryukyu/ie/e185728/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e185728/EnemyTest.java
@@ -6,11 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EnemyTest {
 
-    @Test
-    void attack() {
+    @Test //複数回実行確認してください
+    public void attack() {
         int heroHP = 10;
-        Hero hero = new Hero("testHero", 8, 4);
-        Enemy enemy = new Enemy("testEnemy", 6, 2);
+        Hero hero = new Hero("テスト勇者", heroHP,5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
         enemy.dead = true;
         for (int i = 0; i < 10; i++) {
             enemy.attack(hero);
